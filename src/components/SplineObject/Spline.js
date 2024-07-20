1b2a1e7a88612a9f7079ad712ded82968fb16236
Diya Goyal <dgoyal6_be22@thapar.edu>
2024-07-20 14:41:27 +0530

    edits

--- src/components/SplineObject/Spline.js
+++ src/components/SplineObject/Spline.js
@@ -2,10 +2,17 @@ import Spline from '@splinetool/react-spline';
 
 export default function Object() {
   return (
-    <main>
-      <Spline
+    <div className='spline-container'>
+
+      <Spline className='spline'
         scene="https://prod.spline.design/5k6MSUEt5eF9rvxx/scene.splinecode" 
+        frameBorder='0'
+        width='100%'
+        height='100%'
+        allow='fullscreen'
       />
-    </main>
+      
+    </div>
+    
   );
 }
