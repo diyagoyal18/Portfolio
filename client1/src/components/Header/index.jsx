8e6f7f01865a2b1a3bc695470629dc2c94920f61
Diya Goyal <dgoyal6_be22@thapar.edu>
2024-07-24 02:38:20 +0530

    navbar

--- client1/src/components/Header/index.jsx
+++ client1/src/components/Header/index.jsx
@@ -1,26 +1,22 @@
 'use client'
-
 import styles from './style.module.scss'
 import { useState } from 'react';
-
-
-
+import { AnimatePresence } from 'framer-motion';
+import Nav from '../Nav'
 export default function Home() {
 
-
-
   const [isActive, setIsActive] = useState(false);
-
-
 
   return (
     <>
     <div onClick={() => {setIsActive(!isActive)}} className={styles.button}>
       <div className={`${styles.burger} ${isActive ? styles.burgerActive : ""}`}></div>
+      <AnimatePresence mode="wait">
+
+           {isActive && <Nav/>}
+
+       </AnimatePresence>
     </div>
-
     </>
-
   )
-
 }
