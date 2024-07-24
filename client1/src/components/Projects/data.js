15d2c0fe03ace41361cdf7c887a0fa094fd7bd09
Diya Goyal <dgoyal6_be22@thapar.edu>
2024-07-25 02:22:10 +0530

    projects

---
+++ client1/src/components/Projects/data.js
@@ -0,0 +1,30 @@
+export const projects = [
+    {
+      title: "Richard Gaston",
+      src: "/images/image2.png"
+    },
+    {
+      title: "KangHee Kim",
+      src: "/images/image2.png"
+    },
+    {
+      title: "Inka and Niclas",
+      src: "/images/image2.png"
+    },
+    {
+      title: "Arch McLeish",
+      src: "/images/image2.png"
+    },
+    {
+      title: "Nadir Bucan",
+      src: "/images/image2.png"
+    },
+    {
+      title: "Chandler Bondurant",
+      src: "/images/image2.png"
+    },
+    {
+      title: "Arianna Lago",
+      src: "/images/image2.png"
+    }
+]
